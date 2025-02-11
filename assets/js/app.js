a6ed1dbe38bc088790016ef97fc23f25d5f61d26
Jonathan Djoko <119802868+Girdion@users.noreply.github.com>
2025-02-11 14:04:56 +0700

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,21 @@ document.querySelector(".burger-menu").addEventListener("click", function () {
   const nav = document.querySelector("nav");
   nav.classList.toggle("active");
 });
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 50, density: { enable: true, value_area: 1000 } },
+    color: { value: "#b9ff00" },
+    shape: { type: "circle" },
+    opacity: { value: 0.3, random: true },
+    size: { value: 1.5, random: true },
+    move: { speed: 0.6, direction: "none", random: true, out_mode: "out" },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#b9ff00",
+      opacity: 0.5,
+      width: 0.8,
+    },
+  },
+});
